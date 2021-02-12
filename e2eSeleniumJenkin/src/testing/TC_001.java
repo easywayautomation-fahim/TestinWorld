d9d7864c1d70bbb0655e8b955f5aaa885958aabf
17039 <17039@DESKTOP-ANUBLPN.fios-router.home>
2021-02-11 22:42:48 -0500

    Created automation Project

---
+++ e2eSeleniumJenkin/src/testing/TC_001.java
@@ -0,0 +1,19 @@
+package testing;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_001 {
+	@Test
+	public void testcase1()
+	{  System.setProperty("webdiver.chromedriver", "./Driver/chromedriver.exe");
+		ChromeDriver driver= new ChromeDriver();
+		driver.get ("https://www.facebook.com/");
+		driver.findElementById("email").sendKeys("hello");
+		driver.findElementById("pass").sendKeys("hello");
+		driver.quit();
+		
+	}
+	
+
+}
